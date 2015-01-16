7dbf2090e08293a29ec94a64597a4add6f869a71
hzlu <hzlu2010@163.com>
2015-01-16 23:58:36 +0800

    add deposit_check method

--- lib/luosimao/message.rb
+++ lib/luosimao/message.rb
@@ -2,6 +2,7 @@
 module Luosimao
   module Message
     URL = "https://sms-api.luosimao.com/v1/send.json"
+    DEPOSIT_URL = "https://sms-api.luosimao.com/v1/status.json"
 
     def send(phone, content)
       url = URL.parse URL
@@ -13,6 +14,18 @@ module Luosimao
       https.use_ssl = true
       response = https.start {|socket| socket.request(post)}
       JSON.parse response.body
+    end
+
+    def deposit_check
+      uri = URI(DEPOSIT_URL)
+      req = Net::HTTP::GET.new uri
+      req.basic_auth(Luosimao.username, Luosimao.key)
+
+      res = Net::HTTP.start(uri.host, uri.port,
+        :use_ssl => uri.scheme == 'https') do |http|
+        http.request req
+      end
+      JSON.parse res.body
     end
   end
 end
